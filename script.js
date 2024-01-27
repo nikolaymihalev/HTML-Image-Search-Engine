87466a5bf337da7e5c9e707d88e394a2adda2509
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-27 08:58:39 +0000

    Create search images function

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const accessKey = "2K8hfW67Je071LeRvi-DiNCTuLIBxtzvfWr6DZ7oPuU";
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword="";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+}
