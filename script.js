7fe66f80eafca26ca235e20532229ef7d42ff490
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-28 13:27:35 +0000

    Showing button

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ async function searchImages(){
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink);
     })
+
+    showMoreBtn.style.display="block";
 }
 
 searchForm.addEventListener("submit", (e) => {
