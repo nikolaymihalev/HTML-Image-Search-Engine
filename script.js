11b70c28425de0f0692e10e862dd8c365139a53c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-28 13:32:16 +0000

    Clear first page

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ async function searchImages(){
     const response = await fetch(url);
     const data = await response.json();
 
+    if(page===1){
+        searchResult.innerHTML="";
+    }
+
     const results = data.results;
 
     results.map((result)=>{
